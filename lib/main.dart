b8bd02765c16af79c55714d4c5b9050064c2e3e2
Veco32 <lvecerik@gmail.com>
2023-06-27 15:01:54 +0200

    step 11

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:my_notes/view/notes_view.dart';
 import 'package:my_notes/view/verify_email_view.dart';
 import 'package:my_notes/view/login_view.dart';
 import 'package:my_notes/view/register_view.dart';
-import 'firebase_options.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,16 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentsDirectory implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class CouldnotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldnotDeleteNote implements Exception {}
+
+class CouldNotFindNote implements Exception {}
+class CouldNotUpdateNote implements Exception {}
